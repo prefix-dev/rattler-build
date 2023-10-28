5ae44d8d85c8b6468934d4777bec5bb00e1ab138
Eric Shimizu Karbstein <gr41.j4ck@gmail.com>
2023-10-28 04:18:02 -0300

    refactor: Reimplement `TryConvertNode` for `Test` (#241)

--- src/recipe/parser/test.rs
+++ src/recipe/parser/test.rs
@@ -3,7 +3,7 @@ use serde::Serialize;
 use crate::{
     _partialerror,
     recipe::{
-        custom_yaml::{HasSpan, RenderedNode, TryConvertNode},
+        custom_yaml::{HasSpan, RenderedMappingNode, RenderedNode, TryConvertNode},
         error::{ErrorKind, PartialParsingError},
     },
 };
@@ -58,29 +58,36 @@ impl Test {
 impl TryConvertNode<Test> for RenderedNode {
     fn try_convert(&self, name: &str) -> Result<Test, PartialParsingError> {
         match self {
-            RenderedNode::Mapping(map) => {
-                let mut test = Test::default();
-
-                for (key, value) in map.iter() {
-                    match key.as_str() {
-                        "imports" => test.imports = value.try_convert("imports")?,
-                        "commands" => test.commands = value.try_convert("commands")?,
-                        "requires" => test.requires = value.try_convert("requires")?,
-                        "source_files" => test.source_files = value.try_convert("source_files")?,
-                        "files" => test.files = value.try_convert("files")?,
-                        invalid => Err(_partialerror!(
-                            *key.span(),
-                            ErrorKind::InvalidField(invalid.to_string().into()),
-                            help = format!("expected fields for {name} is one of `imports`, `commands`, `requires`, `source_files`, `files`")
-                        ))?
-                    }
-                }
-
-                Ok(test)
+            RenderedNode::Mapping(map) => map.try_convert(name),
+            RenderedNode::Scalar(_) => {
+                Err(_partialerror!(*self.span(), ErrorKind::ExpectedMapping,))?
             }
-            RenderedNode::Scalar(_) => todo!(),
-            RenderedNode::Sequence(_) => todo!(),
-            RenderedNode::Null(_) => todo!(),
+            RenderedNode::Null(_) => Ok(Test::default()),
+            RenderedNode::Sequence(_) => todo!("Not implemented yet: sequence on Test"),
         }
+    }
+}
+
+impl TryConvertNode<Test> for RenderedMappingNode {
+    fn try_convert(&self, name: &str) -> Result<Test, PartialParsingError> {
+        let mut test = Test::default();
+
+        for (key, value) in self.iter() {
+            let key_str = key.as_str();
+            match key_str {
+                "imports" => test.imports = value.try_convert(key_str)?,
+                "commands" => test.commands = value.try_convert(key_str)?,
+                "requires" => test.requires = value.try_convert(key_str)?,
+                "source_files" => test.source_files = value.try_convert(key_str)?,
+                "files" => test.files = value.try_convert(key_str)?,
+                invalid => Err(_partialerror!(
+                    *key.span(),
+                    ErrorKind::InvalidField(invalid.to_string().into()),
+                    help = format!("expected fields for {name} is one of `imports`, `commands`, `requires`, `source_files`, `files`")
+                ))?
+            }
+        }
+
+        Ok(test)
     }
 }
