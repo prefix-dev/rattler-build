403ee377c1c1900dc5be928a816c0729a29738c4
Wolf Vollprecht <w.vollprecht@gmail.com>
2023-12-19 14:35:35 +0100

    add method to upload to prefix

--- src/upload.rs
+++ src/upload.rs
@@ -1,12 +1,37 @@
-use std::path::PathBuf;
+use std::{path::PathBuf, fmt::Write};
+
+use futures::TryStreamExt;
+use indicatif::{style::TemplateError, HumanBytes, ProgressState};
+use tokio_util::io::ReaderStream;
 
 use miette::{Context, IntoDiagnostic};
 use rattler_conda_types::package::{IndexJson, PackageFile};
+use rattler_digest::compute_file_digest;
 use rattler_networking::{redact_known_secrets_from_error, Authentication, AuthenticationStorage};
 use reqwest::Method;
-use sha2::Digest;
+use sha2::{Digest, Sha256};
 use tracing::info;
 use url::Url;
+
+
+/// Returns the style to use for a progressbar that is currently in progress.
+fn default_bytes_style() -> Result<indicatif::ProgressStyle, TemplateError> {
+    Ok(indicatif::ProgressStyle::default_bar()
+            .template("{spinner:.green} {prefix:20!} [{elapsed_precise}] [{bar:40!.bright.yellow/dim.white}] {bytes:>8} @ {smoothed_bytes_per_sec:8}")?
+            .progress_chars("━━╾─")
+            .with_key(
+                "smoothed_bytes_per_sec",
+                |s: &ProgressState, w: &mut dyn Write| match (s.pos(), s.elapsed().as_millis()) {
+                    (pos, elapsed_ms) if elapsed_ms > 0 => {
+                        // TODO: log with tracing?
+                        _ = write!(w, "{}/s", HumanBytes((pos as f64 * 1000_f64 / elapsed_ms as f64) as u64));
+                    }
+                    _ => {
+                        _ = write!(w, "-");
+                    },
+                },
+            ))
+}
 
 pub async fn upload_package_to_quetz(
     storage: &AuthenticationStorage,
@@ -146,6 +171,83 @@ pub async fn upload_package_to_artifactory(
         .wrap_err("Artifactory responded with error")?;
 
     info!("Package was successfully uploaded to artifactory server");
+
+    Ok(())
+}
+
+pub async fn upload_package_to_prefix(
+    storage: &AuthenticationStorage,
+    api_key: Option<String>,
+    package_file: PathBuf,
+    url: Url,
+    channel: String,
+) -> miette::Result<()> {
+    let filename = package_file
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+
+    let file_size = package_file.metadata().into_diagnostic()?.len();
+
+    println!("Uploading package to: {}", url);
+    println!(
+        "Package file:         {} ({})\n",
+        package_file.display(),
+        HumanBytes(file_size)
+    );
+
+    let client = reqwest::Client::builder()
+        .no_gzip()
+        .build()
+        .expect("failed to create client");
+
+    let sha256sum = format!(
+        "{:x}",
+        compute_file_digest::<Sha256>(&package_file).into_diagnostic()?
+    );
+
+    let file = tokio::fs::File::open(&package_file).await.into_diagnostic()?;
+
+    let progress_bar = indicatif::ProgressBar::new(file_size)
+        .with_prefix("Uploading")
+        .with_style(default_bytes_style().into_diagnostic()?);
+
+    let reader_stream = ReaderStream::new(file)
+        .inspect_ok(move |bytes| {
+            progress_bar.inc(bytes.len() as u64);
+        })
+        .inspect_err(|e| {
+            println!("Error while uploading: {}", e);
+        });
+
+    let body = reqwest::Body::wrap_stream(reader_stream);
+
+    let response = client
+        .post(url.clone())
+        .header("X-File-Sha256", sha256sum)
+        .header("X-File-Name", filename)
+        .header("Content-Length", file_size)
+        .header("Content-Type", "application/octet-stream")
+        .body(body)
+        .send()
+        .await
+        .into_diagnostic()?;
+
+    if response.status().is_success() {
+        println!("Upload successful!");
+    } else {
+        println!("Upload failed!");
+
+        if response.status() == 401 {
+            println!(
+                "Authentication failed! Did you run `pixi auth login {}`?",
+                url
+            );
+        }
+
+        std::process::exit(1);
+    }
 
     Ok(())
 }
