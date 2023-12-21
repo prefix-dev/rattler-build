a5ff5ef5cda3ab5789bdacc95a82eaef6dcac544
Swarnim Arun <swarnimarun11@gmail.com>
2023-12-21 18:34:32 +0530

    feat: tar & zip in rust (#438)

--- src/source/mod.rs
+++ src/source/mod.rs
@@ -1,12 +1,15 @@
 //! Module for fetching sources and applying patches
 
 use std::{
-    path::{Path, PathBuf, StripPrefixError},
-    process::Command,
+    ffi::OsStr,
+    path::{Component, Path, PathBuf, StripPrefixError},
 };
 
 use crate::recipe::parser::Source;
+
 use fs_err as fs;
+use fs_err::File;
+use zip::{result::ZipResult, ZipArchive};
 
 pub mod copy_dir;
 pub mod git_source;
@@ -43,14 +46,17 @@ pub enum SourceError {
     #[error("Could not find `patch` executable")]
     PatchNotFound,
 
-    #[error("Could not find `tar` executable")]
-    TarNotFound,
-
     #[error("Failed to apply patch: {0}")]
     PatchFailed(String),
 
     #[error("Failed to extract archive: {0}")]
-    ExtractionError(String),
+    TarExtractionError(String),
+
+    #[error("Failed to extract zip archive: {0}")]
+    ZipExtractionError(String),
+
+    #[error("Failed to read from zip: {0}")]
+    InvalidZip(String),
 
     #[error("Failed to run git command: {0}")]
     GitError(String),
@@ -122,16 +128,22 @@ pub async fn fetch_sources(
                     fs::create_dir_all(&dest_dir)?;
                 }
 
-                const KNOWN_ARCHIVE_EXTENSIONS: [&str; 5] =
-                    ["tar", "tar.gz", "tar.xz", "tar.bz2", "zip"];
-                if KNOWN_ARCHIVE_EXTENSIONS.iter().any(|ext| {
-                    res.file_name()
-                        .unwrap_or_default()
-                        .to_string_lossy()
-                        .ends_with(ext)
-                }) {
-                    extract(&res, &dest_dir)?;
+                if res
+                    .file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .contains(".tar")
+                {
+                    extract(&res, &dest_dir, 1)?;
                     tracing::info!("Extracted to {:?}", dest_dir);
+                } else if res
+                    .file_name()
+                    .unwrap_or_default()
+                    .to_string_lossy()
+                    .ends_with(".zip")
+                {
+                    extract_zip(&res, &dest_dir, 1)?;
+                    tracing::info!("Extracted zip to {:?}", dest_dir);
                 } else {
                     if let Some(file_name) = src.file_name() {
                         dest_dir = dest_dir.join(file_name);
@@ -193,39 +205,166 @@ pub async fn fetch_sources(
     Ok(())
 }
 
-/// Extracts a tar archive to the specified target directory
-fn extract(archive: &Path, target_directory: &Path) -> Result<std::process::Output, SourceError> {
-    let tar_exe = which::which("tar").map_err(|_| SourceError::TarNotFound)?;
+/// Handle Compression formats internally
+enum TarCompression<'a> {
+    PlainTar(File),
+    Gzip(flate2::read::GzDecoder<File>),
+    Bzip2(bzip2::read::BzDecoder<File>),
+    Xz2(xz2::read::XzDecoder<File>),
+    Zstd(zstd::stream::read::Decoder<'a, std::io::BufReader<File>>),
+    Compress,
+    Lzip,
+    Lzop,
+}
 
-    let mut command = Command::new(&tar_exe);
-    command
-        .arg("-xf")
-        .arg(archive.as_os_str())
-        .arg("--preserve-permissions");
-
-    // zip files don't need to have root directory (they can though, but we can't strip-component
-    // unconditionally as it's generally not the case)
-    if !archive
-        .extension()
-        .map(|ex| ex.eq("zip"))
-        .unwrap_or_default()
+fn ext_to_compression(ext: Option<&OsStr>, file: File) -> TarCompression {
+    match ext
+        .and_then(OsStr::to_str)
+        .and_then(|s| s.rsplit_once('.'))
+        .map(|(_, s)| s)
     {
-        command.arg("--strip-components=1");
+        Some("gz" | "tgz" | "taz") => TarCompression::Gzip(flate2::read::GzDecoder::new(file)),
+        Some("bz2" | "tbz" | "tbz2" | "tz2") => {
+            TarCompression::Bzip2(bzip2::read::BzDecoder::new(file))
+        }
+        Some("lzma" | "tlz" | "xz" | "txz") => TarCompression::Xz2(xz2::read::XzDecoder::new(file)),
+        Some("zst" | "tzst") => {
+            TarCompression::Zstd(zstd::stream::read::Decoder::new(file).unwrap())
+        }
+        Some("Z" | "taZ") => TarCompression::Compress,
+        Some("lz") => TarCompression::Lzip,
+        Some("lzo") => TarCompression::Lzop,
+        Some(_) | None => TarCompression::PlainTar(file),
+    }
+}
+
+impl std::io::Read for TarCompression<'_> {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        match self {
+            TarCompression::PlainTar(reader) => reader.read(buf),
+            TarCompression::Gzip(reader) => reader.read(buf),
+            TarCompression::Bzip2(reader) => reader.read(buf),
+            TarCompression::Xz2(reader) => reader.read(buf),
+            TarCompression::Zstd(reader) => reader.read(buf),
+            TarCompression::Compress | TarCompression::Lzip | TarCompression::Lzop => {
+                todo!("unsupported for now")
+            }
+        }
+    }
+}
+
+/// Extracts a tar archive to the specified target directory
+fn extract(
+    archive: &Path,
+    target_directory: &Path,
+    strip_components: usize,
+) -> Result<(), SourceError> {
+    let mut archive = tar::Archive::new(ext_to_compression(
+        archive.file_name(),
+        File::open(archive).map_err(|_| SourceError::FileNotFound(archive.to_path_buf()))?,
+    ));
+
+    for entry in archive.entries()? {
+        let mut entry = entry?;
+        let mut path = PathBuf::new();
+        {
+            // Essentially from https://github.com/alexcrichton/tar-rs/blob/34744459084c1fffb03d6c742f5a5af9a6403bc4/src/entry.rs#L381
+            // for secure implementation of unpack, we skip all paths with ParentDir component as listed in below CVEs
+            let entrypath = entry.path()?;
+            for part in entrypath.components().skip(strip_components) {
+                match part {
+                    // Leading '/' characters, root paths, and '.'
+                    // components are just ignored and treated as "empty
+                    // components"
+                    Component::Prefix(..) | Component::RootDir | Component::CurDir => continue,
+                    // If any part of the filename is '..', then skip over
+                    // unpacking the file to prevent directory traversal
+                    // security issues.  See, e.g.: CVE-2001-1267,
+                    // CVE-2002-0399, CVE-2005-1918, CVE-2007-4131
+                    Component::ParentDir => continue,
+                    Component::Normal(part) => path.push(part),
+                }
+            }
+        }
+        let path = target_directory.join(path);
+        if entry.header().entry_type().is_dir() {
+            // only errors if fails to create dir
+            // and if file doesn't already exists
+            std::fs::create_dir_all(&path)?;
+            continue;
+        }
+        // create parent dir if doesn't already exists before unpacking
+        if let Some(parent) = path.parent() {
+            if !parent.exists() {
+                std::fs::create_dir_all(parent)?;
+            }
+        }
+        // should setup permissions and xattrs
+        entry.unpack(path)?;
     }
 
-    command.arg("-C").arg(target_directory.as_os_str());
+    Ok(())
+}
 
-    let output = command.output()?;
+/// Extracts a zip archive to the specified target directory
+/// currently this doesn't support bzip2 and zstd, zip archived with compression other than deflate would fail.
+/// <!-- TODO: we can trivially add support for bzip2 and zstd by enabling the feature flags -->
+fn extract_zip(
+    archive: &Path,
+    target_directory: &Path,
+    strip_components: usize,
+) -> Result<(), SourceError> {
+    let archive = zip::ZipArchive::new(
+        File::open(archive).map_err(|_| SourceError::FileNotFound(archive.to_path_buf()))?,
+    )
+    .map_err(|e| SourceError::InvalidZip(e.to_string()))?;
 
-    if !output.status.success() {
-        return Err(SourceError::ExtractionError(format!(
-            "Failed to extract archive with {:?}: {}.\nStdout: {}\nStderr: {}",
-            tar_exe,
-            archive.display(),
-            String::from_utf8_lossy(&output.stdout),
-            String::from_utf8_lossy(&output.stderr)
-        )));
+    extract_zip_stripped(archive, target_directory, strip_components)
+        .map_err(|e| SourceError::ZipExtractionError(e.to_string()))?;
+
+    Ok(())
+}
+
+fn extract_zip_stripped(
+    mut zip: ZipArchive<File>,
+    target_directory: &Path,
+    strip_components: usize,
+) -> ZipResult<()> {
+    use std::fs;
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i)?;
+        let filepath = file
+            .enclosed_name()
+            .ok_or(zip::result::ZipError::InvalidArchive("Invalid file path"))?;
+
+        let filepath = filepath
+            .components()
+            .skip(strip_components)
+            .collect::<PathBuf>();
+        if filepath.as_os_str().is_empty() {
+            continue;
+        }
+        let outpath = target_directory.join(filepath);
+
+        if file.name().ends_with('/') {
+            fs::create_dir_all(&outpath)?;
+        } else {
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(p)?;
+                }
+            }
+            let mut outfile = fs::File::create(&outpath)?;
+            std::io::copy(&mut file, &mut outfile)?;
+        }
+        // set permissions
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))?;
+            }
+        }
     }
-
-    Ok(output)
+    Ok(())
 }
