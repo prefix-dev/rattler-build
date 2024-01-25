306aa46df1d0121fca73f6612ab6687c949e57c4
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2024-01-25 18:07:50 +0300

    revert: update curl recipe

--- examples/curl/build.sh
+++ examples/curl/build.sh
@@ -4,17 +4,17 @@
 cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 if [[ $target_platform =~ linux.* ]]; then
-	USESSL="--with-openssl=${PREFIX}"
+    USESSL="--with-openssl=${PREFIX}"
 else
-	USESSL="--with-secure-transport"
-fi
+    USESSL="--with-secure-transport"
+fi;
 
 ./configure \
-	--prefix=${PREFIX} \
-	--host=${HOST} \
-	${USESSL} \
-	--with-ca-bundle=${PREFIX}/ssl/cacert.pem \
-	--disable-static --enable-shared
+    --prefix=${PREFIX} \
+    --host=${HOST} \
+    ${USESSL} \
+    --with-ca-bundle=${PREFIX}/ssl/cacert.pem \
+    --disable-static --enable-shared
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
