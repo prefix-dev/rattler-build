e9188eb1e5ceabfb1c2658e78f03566dbfbc7c43
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-01-25 16:40:13 +0100

    fix tests

--- src/linux/link.rs
+++ src/linux/link.rs
@@ -342,6 +342,7 @@ fn relink(elf_path: &Path, new_rpath: &[PathBuf]) -> Result<(), RelinkError> {
 #[cfg(test)]
 mod test {
     use super::*;
+    use globset::{Glob, GlobSetBuilder};
     use std::{fs, path::Path};
     use tempfile::tempdir_in;
 
@@ -353,14 +354,22 @@ mod test {
     // prefix: "test-data/binary_files"
     // new rpath: $ORIGIN/../lib
     #[test]
-    #[cfg(target_os = "linux")]
     fn relink_patchelf() -> Result<(), RelinkError> {
-        use globset::Glob;
+        if which::which("patchelf").is_err() {
+            tracing::warn!("patchelf not found, skipping test");
+            return Ok(());
+        }
+
         // copy binary to a temporary directory
         let prefix = Path::new(env!("CARGO_MANIFEST_DIR")).join("test-data/binary_files");
         let tmp_dir = tempdir_in(&prefix)?.into_path();
         let binary_path = tmp_dir.join("zlink");
         fs::copy(prefix.join("zlink"), &binary_path)?;
+
+        let globset = GlobSetBuilder::new()
+            .add(Glob::new("/usr/lib/custom**").unwrap())
+            .build()
+            .unwrap();
 
         // default rpaths of the test binary are:
         // - /rattler-build_zlink/host_env_placehold/lib
@@ -368,12 +377,7 @@ mod test {
         // so we are expecting it to keep the host prefix and discard the build prefix
         let encoded_prefix = Path::new("/rattler-build_zlink/host_env_placehold");
         let object = SharedObject::new(&binary_path)?;
-        object.relink(
-            &prefix,
-            encoded_prefix,
-            &[],
-            &[Glob::new("/usr/lib/custom**").unwrap().compile_matcher()],
-        )?;
+        object.relink(&prefix, encoded_prefix, &[], Some(&globset))?;
         let object = SharedObject::new(&binary_path)?;
         assert_eq!(
             vec!["$ORIGIN/../lib", "/usr/lib/custom_lib"],
@@ -397,8 +401,12 @@ mod test {
     // prefix: "test-data/binary_files"
     // new rpath: $ORIGIN/../lib
     #[test]
-    #[cfg(target_os = "linux")]
     fn relink_add_rpath() -> Result<(), RelinkError> {
+        if which::which("patchelf").is_err() {
+            tracing::warn!("patchelf not found, skipping test");
+            return Ok(());
+        }
+
         // copy binary to a temporary directory
         let prefix = Path::new(env!("CARGO_MANIFEST_DIR")).join("test-data/binary_files");
         let tmp_dir = tempdir_in(&prefix)?.into_path();
@@ -407,7 +415,7 @@ mod test {
 
         let encoded_prefix = Path::new("/rattler-build_zlink/host_env_placehold");
         let object = SharedObject::new(&binary_path)?;
-        object.relink(&prefix, encoded_prefix, &[String::from("lib/")], &[])?;
+        object.relink(&prefix, encoded_prefix, &[String::from("lib/")], None)?;
         let object = SharedObject::new(&binary_path)?;
         assert_eq!(
             vec!["$ORIGIN/../lib"],
