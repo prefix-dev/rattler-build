461a63eecb306b50f364f2eaac087f69efc4537f
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-02-01 17:17:31 +0100

    fix tests on windows

--- test-data/recipes/python_compilation/recipe.py
+++ test-data/recipes/python_compilation/recipe.py
@@ -1,1 +1,21 @@
-print("hello world")
+import os
+from pathlib import Path
+
+prefix = Path(os.environ["PREFIX"])
+
+py_content = "print('Hello, world!')"
+py_file = prefix / "test.py"
+
+py_file.write_text(py_content)
+
+py_skippy = prefix / "test_skippy.py"
+py_skippy.write_text(py_content)
+
+new_folder = prefix / "cmd"
+new_folder.mkdir(exist_ok=True)
+
+py_file = prefix / "cmd" / "test.py"
+py_file.write_text(py_content)
+
+py_broken = prefix / "broken.py"
+py_broken.write_text("print('Hello, world!'")
