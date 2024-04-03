9ec177d4a8efc99dd43b0ecadb27e40eeac99b5f
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-04-03 11:24:33 -0400

    fix: apply patches in target_directory (#760)

--- test/end-to-end/test_simple.py
+++ test/end-to-end/test_simple.py
@@ -583,7 +583,7 @@ def test_git_submodule(rattler_build: RattlerBuild, recipes: Path, tmp_path: Pat
         tmp_path,
     )
 
-    output = check_output([str(rattler_build.path), *args], stderr=STDOUT, text=True)
+    _ = check_output([str(rattler_build.path), *args], stderr=STDOUT, text=True)
     pkg = get_extracted_package(tmp_path, "nanobind")
 
     assert (pkg / "info/paths.json").exists()
@@ -600,3 +600,32 @@ def test_git_submodule(rattler_build: RattlerBuild, recipes: Path, tmp_path: Pat
     source = sources[0]
     assert source["git"] == "https://github.com/wjakob/nanobind/"
     assert source["rev"] == "8e1f8408b37d994fb987440859eb977af39be8c3"
+
+
+@pytest.mark.skipif(
+    os.name == "nt", reason="recipe does not support execution on windows"
+)
+def test_git_patch(rattler_build: RattlerBuild, recipes: Path, tmp_path: Path):
+    path_to_recipe = recipes / "git_source_patch"
+    args = rattler_build.build_args(
+        path_to_recipe,
+        tmp_path,
+    )
+
+    _ = check_output([str(rattler_build.path), *args], stderr=STDOUT, text=True)
+    pkg = get_extracted_package(tmp_path, "ament_package")
+
+    assert (pkg / "info/paths.json").exists()
+    assert (pkg / "info/recipe/rendered_recipe.yaml").exists()
+    # load recipe as YAML
+
+    text = (pkg / "info/recipe/rendered_recipe.yaml").read_text()
+
+    # parse the rendered recipe
+    rendered_recipe = yaml.safe_load(text)
+    sources = rendered_recipe["finalized_sources"]
+
+    assert len(sources) == 1
+    source = sources[0]
+    assert source["git"] == "https://github.com/ros2-gbp/ament_package-release.git"
+    assert source["rev"] == "00da147b17c19bc225408dc693ed8fdc14c314ab"
