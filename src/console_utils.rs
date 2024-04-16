100bf76b509633a98b0434a9cc5cd82a01ed397e
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-04-16 14:00:07 -0400

    fix: improved overlink warnings and prepare for linking checks on Windows (#688)

--- src/console_utils.rs
+++ src/console_utils.rs
@@ -481,10 +481,15 @@ impl<S: Subscriber> Layer<S> for GitHubActionsLayer {
             return;
         }
         let metadata = event.metadata();
+
+        let mut message = Vec::new();
+        event.record(&mut CustomVisitor::new(&mut message));
+        let message = String::from_utf8_lossy(&message);
+
         match *metadata.level() {
-            Level::WARN => println!("::warning ::{}", format_args!("{:?}", event)),
-            Level::ERROR => println!("::error ::{}", format_args!("{:?}", event)),
-            _ => {} // Ignore other levels
+            Level::ERROR => println!("::error ::{}", message),
+            Level::WARN => println!("::warning ::{}", message),
+            _ => {}
         }
     }
 }
