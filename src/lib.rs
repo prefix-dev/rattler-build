d680b2b0a04f5eaf7c7b44c33bc657c44b5a3940
Bas Zalmstra <zalmstra.bas@gmail.com>
2024-05-28 15:17:29 +0200

    fix: clippy

--- src/lib.rs
+++ src/lib.rs
@@ -257,7 +257,7 @@ pub async fn get_build_output(
             .clone()
             .unwrap_or_else(|| vec!["conda-forge".to_string()])
             .into_iter()
-            .map(|c| Channel::from_str(&c, &tool_config.channel_config).map(|c| c.base_url))
+            .map(|c| Channel::from_str(c, &tool_config.channel_config).map(|c| c.base_url))
             .collect::<Result<Vec<_>, _>>()
             .into_diagnostic()?;
 
@@ -363,7 +363,7 @@ pub async fn run_test_from_args(
         .channel
         .unwrap_or_else(|| vec!["conda-forge".to_string()])
         .into_iter()
-        .map(|name| Channel::from_str(&name, &channel_config).map(|c| c.base_url))
+        .map(|name| Channel::from_str(name, &channel_config).map(|c| c.base_url))
         .collect::<Result<Vec<_>, _>>()
         .into_diagnostic()?;
 
