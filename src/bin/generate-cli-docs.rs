63b6a07592922e438f71a3ef127a78af1e32d45b
Ruben Arts <ruben.arts@hotmail.com>
2024-05-30 11:01:36 +0200

    fmt: cargo fmt --all

--- src/bin/generate-cli-docs.rs
+++ src/bin/generate-cli-docs.rs
@@ -1,7 +1,5 @@
-
 /// This is a separate binary not included in the main rattler-build binary.
 /// Used to generate the documentation for the rattler-build binary.
-
 
 #[cfg(feature = "generate-cli-docs")]
 fn main() {
