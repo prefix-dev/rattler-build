9bda1e875ff6fa8f77ec361bff09c4447bb6b052
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-06-04 11:03:11 +0200

    fix generated cli

--- src/bin/generate-cli-docs.rs
+++ src/bin/generate-cli-docs.rs
@@ -1,12 +1,18 @@
 /// This is a separate binary not included in the main rattler-build binary.
 /// Used to generate the documentation for the rattler-build binary.
+use rattler_conda_types::Platform;
 
 #[cfg(feature = "generate-cli-docs")]
 fn main() {
-    use clap_markdown::print_help_markdown;
+    use clap_markdown::help_markdown;
     use rattler_build::opt::App;
 
-    print_help_markdown::<App>();
+    let help = help_markdown::<App>();
+
+    let target_default_platform = format!("Default value: `{}`", Platform::current());
+    let help = help.replace(target_default_platform.as_str(), format!("Default value: current platform").as_str());
+
+    print!("{}", help);
 }
 
 #[cfg(not(feature = "generate-cli-docs"))]
