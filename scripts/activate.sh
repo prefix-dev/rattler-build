b77d3703b2d899c4f48c09e17ba9a8b3296610f5
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-11-09 17:44:25 +0100

    add flags for rpath on macOS

--- scripts/activate.sh
+++ scripts/activate.sh
@@ -3,3 +3,8 @@ set -Eeuo pipefail
 export CARGO_TARGET_DIR="target-pixi"
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_LINKER="clang"
 export CARGO_TARGET_X86_64_UNKNOWN_LINUX_GNU_RUSTFLAGS="-C link-arg=-fuse-ld=$CONDA_PREFIX/bin/mold"
+
+# on macOS we need to set these rust flags:
+if [[ uname == "Darwin" ]]; then
+    export CARGO_BUILD_RUSTFLAGS="-C link-arg=-Wl,-rpath,$CONDA_PREFIX/lib"
+fi
