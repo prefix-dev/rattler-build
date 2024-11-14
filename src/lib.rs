5e83d1923182660ff9f0b8226387f1d545d2c2e1
Hadrien Mary <hadim@users.noreply.github.com>
2024-11-14 11:18:26 -0500

    feat: skip noarch build if `--noarch-build-platform` != `build_platform` (#1192)

--- src/lib.rs
+++ src/lib.rs
@@ -134,6 +134,7 @@ pub fn get_tool_config(
         .with_zstd_repodata_enabled(args.common.use_zstd)
         .with_bz2_repodata_enabled(args.common.use_zstd)
         .with_skip_existing(args.skip_existing)
+        .with_noarch_build_platform(args.noarch_build_platform)
         .finish())
 }
 
@@ -408,6 +409,35 @@ pub async fn run_build_from_args(
     }
 
     Ok(())
+}
+
+/// Check if the noarch builds should be skipped because the noarch platform has been set
+pub async fn skip_noarch(
+    mut outputs: Vec<Output>,
+    tool_configuration: &tool_configuration::Configuration,
+) -> miette::Result<Vec<Output>> {
+    if let Some(noarch_build_platform) = tool_configuration.noarch_build_platform {
+        outputs.retain(|output| {
+            // Skip the build if:
+            // - target_platform is "noarch"
+            // and
+            // - build_platform != noarch_build_platform
+            let should_skip = output.build_configuration.target_platform == Platform::NoArch
+                && output.build_configuration.build_platform.platform != noarch_build_platform;
+
+            if should_skip {
+                // The identifier should always be set at this point
+                tracing::info!(
+                    "Skipping build because noarch_build_platform is set to {} for {}",
+                    noarch_build_platform,
+                    output.identifier()
+                );
+            }
+            !should_skip
+        });
+    }
+
+    Ok(outputs)
 }
 
 /// Runs test.
