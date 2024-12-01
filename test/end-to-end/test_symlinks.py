7c568ef8c496213eb3943beeabe545e80d87e46f
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-12-01 10:54:26 +0100

    use ruff / ruff-format

--- test/end-to-end/test_symlinks.py
+++ test/end-to-end/test_symlinks.py
@@ -36,7 +36,12 @@ def test_symlink_cache(
     paths_json = pkg / "info/paths.json"
     j = json.loads(paths_json.read_text())
     # prefix placeholder always changes, and we test it later
-    assert snapshot_json(exclude=filter_paths("paths.4.prefix_placeholder", "paths.4.sha256")) == j
+    assert (
+        snapshot_json(
+            exclude=filter_paths("paths.4.prefix_placeholder", "paths.4.sha256")
+        )
+        == j
+    )
 
     paths = j["paths"]
     assert len(paths) == 6
