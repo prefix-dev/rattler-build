8b9596b55dfd3a2a3377020bc5d29730a805573a
Wolf Vollprecht <w.vollprecht@gmail.com>
2024-12-04 10:49:00 +0100

    use ruff

--- py-rattler-build/rattler_build/__init__.py
+++ py-rattler-build/rattler_build/__init__.py
@@ -1,5 +1,4 @@
-from .rattler_build import \
-    get_rattler_build_version_py as _get_rattler_build_version_py
+from .rattler_build import get_rattler_build_version_py as _get_rattler_build_version_py
 
 
 def rattler_build_version() -> str:
