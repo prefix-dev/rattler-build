0ee7451bb9f32b76a3d2f4af0b43823a3adce0bd
Thijs <35424969+hiphof@users.noreply.github.com>
2024-12-10 21:36:57 +0100

    feat: add R interpreter

---
+++ src/script/interpreter/r.rs
@@ -0,0 +1,36 @@
+use std::path::PathBuf;
+
+use rattler_conda_types::Platform;
+
+use crate::script::{ExecutionArgs, ResolvedScriptContents};
+
+use super::{find_interpreter, BashInterpreter, CmdExeInterpreter, Interpreter};
+
+pub(crate) struct RInterpreter;
+
+// R interpreter calls either bash or cmd.exe interpreter for activation and then runs R script
+impl Interpreter for RInterpreter {
+    async fn run(&self, args: ExecutionArgs) -> Result<(), std::io::Error> {
+        let r_script = args.work_dir.join("conda_build_script.r");
+        tokio::fs::write(&r_script, args.script.script()).await?;
+
+        let args = ExecutionArgs {
+            script: ResolvedScriptContents::Inline(format!("r {:?}", r_script)),
+            ..args
+        };
+
+        if cfg!(windows) {
+            CmdExeInterpreter.run(args).await
+        } else {
+            BashInterpreter.run(args).await
+        }
+    }
+
+    async fn find_interpreter(
+        &self,
+        build_prefix: Option<&PathBuf>,
+        platform: &Platform,
+    ) -> Result<Option<PathBuf>, which::Error> {
+        find_interpreter("r", build_prefix, platform)
+    }
+}
