233f8ccfde7fb8e56082e3468561731a4fc81a61
Julian Hofer <julianhofer@gnome.org>
2025-01-10 10:59:29 +0100

    fix: version setup

--- py-rattler-build/tests/unit/test_basic.py
+++ py-rattler-build/tests/unit/test_basic.py
@@ -1,14 +1,16 @@
 from pathlib import Path
 
 import rattler_build
+import tomli as tomllib
 
 
-def test_basic() -> None:
-    parent_cargo_toml = Path(__file__).parent.parent.parent.parent / "Cargo.toml"
-    # get the version with a regex
-    text = parent_cargo_toml.read_text()
-    for line in text.splitlines():
-        if line.startswith("version"):
-            version = line.split("=")[1].strip().strip('"')
-            break
-    assert rattler_build.rattler_build_version() == version
+def test_version_match_parent_cargo() -> None:
+    parent_cargo_toml = Path(__file__).parents[3].joinpath("Cargo.toml").read_text()
+    parent_version = tomllib.loads(parent_cargo_toml)["package"]["version"]
+    assert rattler_build.rattler_build_version() == parent_version
+
+
+def test_version_match_local_cargo() -> None:
+    local_cargo_toml = Path(__file__).parents[2].joinpath("Cargo.toml").read_text()
+    local_version = tomllib.loads(local_cargo_toml)["package"]["version"]
+    assert rattler_build.rattler_build_version() == local_version
