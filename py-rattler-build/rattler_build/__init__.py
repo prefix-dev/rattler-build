61d02fde82fe9f221a8bb5d07bb2b58108cf053c
Julian Hofer <julianhofer@gnome.org>
2025-01-14 10:17:47 +0100

    Default value for output dir

--- py-rattler-build/rattler_build/__init__.py
+++ py-rattler-build/rattler_build/__init__.py
@@ -9,7 +9,7 @@ def rattler_build_version() -> str:
     return get_rattler_build_version_py()
 
 
-def build_recipe(recipe_path: Union[str, Path], output_dir: Union[str, Path, None]) -> None:
+def build_recipe(recipe_path: Union[str, Path], output_dir: Union[str, Path, None] = None) -> None:
     output_dir = None if output_dir is None else str(output_dir)
     recipes = [str(recipe_path)]
     build_recipes_py(recipes, output_dir)
