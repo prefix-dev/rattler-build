c110442aeabd8256289bf12810c4f4be306f18f1
Daniel Elsner <daniel.elsner@quantco.com>
2025-03-11 23:14:39 +0100

    fix

--- test/end-to-end/test_simple.py
+++ test/end-to-end/test_simple.py
@@ -328,7 +328,6 @@ def test_s3_minio_upload(
         str(get_package(tmp_path, "globtest")),
     ]
     rattler_build(*cmd)
-    breakpoint()
 
     # Check if package was correctly uploaded
     package_key = f"{s3_config.channel_name}/{host_subdir()}/globtest-0.24.6-{variant_hash({'target_platform': host_subdir()})}_0.tar.bz2"
