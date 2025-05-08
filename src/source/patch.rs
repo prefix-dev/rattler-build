2fbed4616ae0e025e4bca9090e4b5cfaab3befc0
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-05-08 16:54:31 +0200

    fix: mixed line ending (CRLF vs LF) issues with patches (#1627)
    Co-authored-by: Denizhan Dakılır <denizhandakilirn@gmail.com>

--- src/source/patch.rs
+++ src/source/patch.rs
@@ -1,7 +1,9 @@
 //! Functions for applying patches to a work directory.
 use std::{
+    io::Write,
     ops::Deref,
     path::{Path, PathBuf},
+    process::Stdio,
 };
 
 use gitpatch::Patch;
@@ -52,33 +54,63 @@ pub(crate) fn apply_patches(
     work_dir: &Path,
     recipe_dir: &Path,
 ) -> Result<(), SourceError> {
-    for patch in patches {
-        let patch = recipe_dir.join(patch);
+    for patch_path_relative in patches {
+        let patch_file_path = recipe_dir.join(patch_path_relative);
 
-        tracing::info!("Applying patch: {}", patch.to_string_lossy());
+        tracing::info!("Applying patch: {}", patch_file_path.to_string_lossy());
 
-        if !patch.exists() {
-            return Err(SourceError::PatchNotFound(patch));
+        if !patch_file_path.exists() {
+            return Err(SourceError::PatchNotFound(patch_file_path));
         }
 
-        let strip_level = guess_strip_level(&patch, work_dir)?;
+        // Read the patch content into a string. This also normalizes line endings to LF.
+        let patch_content_for_stdin =
+            fs_err::read_to_string(&patch_file_path).map_err(SourceError::Io)?;
 
-        let output = system_tools
-            .call(Tool::Patch)
-            .map_err(|_| SourceError::PatchExeNotFound)?
+        let strip_level = guess_strip_level(&patch_file_path, work_dir)?;
+
+        let mut cmd_builder = system_tools
+            .call(Tool::Git)
+            .map_err(SourceError::GitNotFound)?;
+
+        cmd_builder
+            .current_dir(work_dir)
+            .arg("apply")
             .arg(format!("-p{}", strip_level))
-            .arg("-i")
-            .arg(String::from(patch.to_string_lossy()))
-            .arg("-d")
-            .arg(String::from(work_dir.to_string_lossy()))
-            .output()?;
+            .arg("--ignore-space-change")
+            .arg("--ignore-whitespace")
+            .arg("--recount")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped());
+
+        let mut child_process = cmd_builder.spawn().map_err(SourceError::Io)?;
+
+        // Write the patch content to the child process's stdin.
+        {
+            if let Some(mut child_stdin) = child_process.stdin.take() {
+                child_stdin
+                    .write_all(patch_content_for_stdin.as_bytes())
+                    .map_err(SourceError::Io)?;
+            } else {
+                return Err(SourceError::Io(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Failed to obtain stdin handle for git apply",
+                )));
+            }
+        }
+
+        let output = child_process.wait_with_output().map_err(SourceError::Io)?;
 
         if !output.status.success() {
-            tracing::error!("Failed to apply patch: {}", patch.to_string_lossy());
-            tracing::error!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
-            tracing::error!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
+            eprintln!(
+                "Failed to apply patch: {}",
+                patch_file_path.to_string_lossy()
+            );
+            eprintln!("Stdout: {}", String::from_utf8_lossy(&output.stdout));
+            eprintln!("Stderr: {}", String::from_utf8_lossy(&output.stderr));
             return Err(SourceError::PatchFailed(
-                patch.to_string_lossy().to_string(),
+                patch_file_path.to_string_lossy().to_string(),
             ));
         }
     }
@@ -87,8 +119,12 @@ pub(crate) fn apply_patches(
 
 #[cfg(test)]
 mod tests {
+    use crate::source::copy_dir::CopyDir;
+
     use super::*;
     use gitpatch::Patch;
+    use line_ending::LineEnding;
+    use tempfile::TempDir;
 
     #[test]
     fn test_parse_patch() {
@@ -108,5 +144,76 @@ mod tests {
 
             println!("Parsing patch: {} {}", patch_path.display(), parsed.is_ok());
         }
+    }
+
+    fn setup_patch_test_dir() -> (TempDir, PathBuf) {
+        let manifest_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        let patch_test_dir = manifest_dir.join("test-data/patch_application");
+
+        let tempdir = TempDir::new().unwrap();
+        let _ = CopyDir::new(&patch_test_dir, tempdir.path()).run().unwrap();
+
+        (tempdir, patch_test_dir)
+    }
+
+    #[test]
+    fn test_apply_patches() {
+        let (tempdir, _) = setup_patch_test_dir();
+
+        // Test with normal patch
+        apply_patches(
+            &SystemTools::new(),
+            &[PathBuf::from("test.patch")],
+            &tempdir.path().join("workdir"),
+            &tempdir.path().join("patches"),
+        )
+        .unwrap();
+
+        let text_md = tempdir.path().join("workdir/text.md");
+        let text_md = fs_err::read_to_string(&text_md).unwrap();
+        assert!(text_md.contains("Oh, wow, I was patched! Thank you soooo much!"));
+    }
+
+    #[test]
+    fn test_apply_patches_with_crlf() {
+        let (tempdir, _) = setup_patch_test_dir();
+
+        // Test with CRLF patch
+        let patch = tempdir.path().join("patches/test.patch");
+        let text = fs_err::read_to_string(&patch).unwrap();
+        let clrf_patch = LineEnding::CRLF.apply(&text);
+
+        fs_err::write(tempdir.path().join("patches/test_crlf.patch"), clrf_patch).unwrap();
+
+        // Test with CRLF patch
+        apply_patches(
+            &SystemTools::new(),
+            &[PathBuf::from("test_crlf.patch")],
+            &tempdir.path().join("workdir"),
+            &tempdir.path().join("patches"),
+        )
+        .unwrap();
+
+        let text_md = tempdir.path().join("workdir/text.md");
+        let text_md = fs_err::read_to_string(&text_md).unwrap();
+        assert!(text_md.contains("Oh, wow, I was patched! Thank you soooo much!"));
+    }
+
+    #[test]
+    fn test_apply_0001_increase_minimum_cmake_version_patch() {
+        let (tempdir, _) = setup_patch_test_dir();
+
+        apply_patches(
+            &SystemTools::new(),
+            &[PathBuf::from("0001-increase-minimum-cmake-version.patch")],
+            &tempdir.path().join("workdir"),
+            &tempdir.path().join("patches"),
+        )
+        .expect("Patch 0001-increase-minimum-cmake-version.patch should apply successfully");
+
+        // Read the cmake list file and make sure that it contains `cmake_minimum_required(VERSION 3.12)`
+        let cmake_list = tempdir.path().join("workdir/CMakeLists.txt");
+        let cmake_list = fs_err::read_to_string(&cmake_list).unwrap();
+        assert!(cmake_list.contains("cmake_minimum_required(VERSION 3.12)"));
     }
 }
