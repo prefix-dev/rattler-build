1ac84c570f04db2347f5ac263e25861f9a366430
Wolf Vollprecht <w.vollprecht@gmail.com>
2025-06-19 11:37:39 +0200

    use rattler-config

--- src/package_test/run_test.rs
+++ src/package_test/run_test.rs
@@ -473,8 +473,7 @@ pub async fn run_test(
 
         for test in tests {
             let test_prefix =
-                TempDir::with_prefix_in(format!("test_{}", pkg.name), &config.test_prefix)?
-                    .keep();
+                TempDir::with_prefix_in(format!("test_{}", pkg.name), &config.test_prefix)?.keep();
             match test {
                 TestType::Command(c) => {
                     c.run_test(&pkg, &package_folder, &test_prefix, &config, &env)
