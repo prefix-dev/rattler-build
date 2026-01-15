eb230541a13ff4f71c4ef766b9d6382d4ac17736
PaperChalice <liujunchang97@outlook.com>
2026-01-15 11:27:12 +0800

    Show warning if powershell is not new enough. It would be easier if semver is available.

--- src/script/interpreter/powershell.rs
+++ src/script/interpreter/powershell.rs
@@ -1,4 +1,5 @@
 use std::path::PathBuf;
+use std::process::Command;
 
 use rattler_conda_types::Platform;
 
@@ -23,14 +24,47 @@ foreach ($envVar in Get-ChildItem Env:) {
 // PowerShell interpreter calls cmd.exe interpreter for activation and then runs PowerShell script
 impl Interpreter for PowerShellInterpreter {
     async fn run(&self, args: ExecutionArgs) -> Result<(), InterpreterError> {
+        let mut shell_cmd = "pwsh";
+        let mut new_enough = true;
+        let result: Option<()> = which::which("pwsh").ok().and_then(|_| {
+            let out =
+                String::from_utf8(Command::new("pwsh").arg("-v").output().ok()?.stdout).ok()?;
+            let ver = out
+                .trim()
+                .split(' ')
+                .last()?
+                .split('.')
+                .collect::<Vec<&str>>();
+            if ver.len() < 2 {
+                return None;
+            }
+
+            let major = ver[0].parse::<i32>().ok()?;
+            let minor = ver[1].parse::<i32>().ok()?;
+            if major < 7 || (major == 7 && minor < 4) {
+                new_enough = false;
+            }
+
+            return Some(());
+        });
+        if result.is_none() {
+            shell_cmd = "powershell";
+            new_enough = false;
+        }
+        if !new_enough {
+            eprintln!(
+                "Warning: rattler-build requires PowerShell 7.4+, otherwise it will skip native command errors!"
+            );
+        }
+
         let ps1_script = args.work_dir.join("conda_build_script.ps1");
         let contents = POWERSHELL_PREAMBLE.to_owned() + args.script.script();
         tokio::fs::write(&ps1_script, contents).await?;
 
         let args = ExecutionArgs {
             script: ResolvedScriptContents::Inline(format!(
-                "pwsh -NoLogo -NoProfile {:?}",
-                ps1_script
+                "{} -NoLogo -NoProfile {:?}",
+                shell_cmd, ps1_script
             )),
             ..args
         };
