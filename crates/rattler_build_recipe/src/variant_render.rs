f103621ab452c91495e25d3733503cec99589fd5
Wolf Vollprecht <w.vollprecht@gmail.com>
2026-09-09 18:19:36 +0200

    fix: always use CROSSCOMPILING_EMULATOR variant

--- crates/rattler_build_recipe/src/variant_render.rs
+++ crates/rattler_build_recipe/src/variant_render.rs
@@ -3803,6 +3803,37 @@ postgresql:
     }
 
     #[test]
+    fn test_crosscompiling_emulator_variant_is_always_used() {
+        let recipe_yaml = r#"
+package:
+  name: test-pkg
+  version: "1.0.0"
+"#;
+        let variant_yaml = r#"
+CROSSCOMPILING_EMULATOR:
+  - qemu-aarch64
+  - qemu-x86_64
+"#;
+        let stage0_recipe = stage0::parse_recipe_or_multi_from_source(recipe_yaml).unwrap();
+        let variant_config = VariantConfig::from_yaml_str(variant_yaml).unwrap();
+
+        let rendered = render_recipe_with_variant_config(
+            &stage0_recipe,
+            &variant_config,
+            RenderConfig::new()
+                .with_os_env_var_keys(HashSet::from(["CROSSCOMPILING_EMULATOR".to_string()])),
+        )
+        .unwrap();
+
+        assert_eq!(rendered.len(), 2);
+        assert!(
+            rendered
+                .iter()
+                .all(|r| r.variant.contains_key(&"CROSSCOMPILING_EMULATOR".into()))
+        );
+    }
+
+    #[test]
     fn test_build_string_prefix_simple() {
         let recipe_yaml = r#"
 package:
